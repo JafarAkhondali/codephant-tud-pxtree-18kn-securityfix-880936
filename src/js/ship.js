9168eec4b9079d2d2ac1a8db5fc8213840a9aa55
André Arnold <andre.arnold@japaninleipzig.de>
2014-05-13 23:58:22 +0200

    fix: idle and move animation can coexist.

--- src/js/ship.js
+++ src/js/ship.js
@@ -5,59 +5,61 @@ namespace("PXTree.AchtzehnKnoten", function (AzK)
 	{
 		this.game = game;
 		this.sprite = null;
-		this.model = {speed: 0.1};
-		this.idling = null;
-		this.currentTween = null;
+		this.model = {speed: 0.2};
 		this.yOffset = { v: 0, up: true };
+		this._coordDelta = new Phaser.Point();
 	};
 	
 	AzK.Ship.prototype =
-			{ preload: function ()
+			{ FLOAT_SPEED_FACTOR: 2.5
+			
+			, preload: function ()
 				{
 					this.game.load.image('ship', 'assets/schiff-64x64-2x.png');
 				}
 	
 			, create: function ()
 				{
-					this.sprite = this.game.make.sprite(0, 0, 'ship');
-					this.game.world.add(this.sprite);
-					this.idle();
+					this.sprite = this.game.add.sprite(0, 0, 'ship');
+					this._coordDelta.set(
+							this.sprite.width * (4/5),
+							this.sprite.height * (4/5));
 				}
 			
 			, update: function ()
 				{
+					this._floatStep();
 				}
 			
-			, move: function (toPort, withoutTween)
+			, move: function (toPort, instant)
 				{
-					var ship = this.sprite
-						, dest = new Phaser.Point(toPort.x, toPort.y)
-								.subtract(ship.width * (3/4), ship.height * (3/4))
-						, tween
+					var dest = new Phaser.Point.subtract(toPort, this._coordDelta)
 						;
-					this.currentTween && this.currentTween.stop();
-					if (withoutTween)
-					{
+					if (instant)
 						this.sprite.position.copyFrom(dest);
+					else
+						this.game.add.tween(this.sprite)
+								.to(dest,
+									this.sprite.position.distance(dest) / this.model.speed,
+									Phaser.Easing.Quadratic.InOut).start();
+				}
+
+			, _floatStep: function ()
+				{
+					//the floating motion is reverse after sqrt(16)=4 pixels.
+					if (this.yOffset.v * this.yOffset.v >= 16)
+						this.yOffset.up = !this.yOffset.up;
+					
+					if (this.yOffset.up)
+					{
+						this.yOffset.v += this.FLOAT_SPEED_FACTOR * this.model.speed;
+						this.sprite.y -= this.FLOAT_SPEED_FACTOR * this.model.speed;
 					}
 					else
 					{
-						tween = this.game.add.tween(this.sprite);
-						this.currentTween = tween;
-						
-						tween.to({x: dest.x, y:dest.y},
-								dest.distance({x:toPort.x, y:toPort.y}) / this.model.speed,
-								Phaser.Easing.Quadratic.InOut);
-						tween.onComplete.add(function(){ this.idle(); }, this);
-						tween.start();
+						this.yOffset.v -= this.FLOAT_SPEED_FACTOR * this.model.speed;
+						this.sprite.y += this.FLOAT_SPEED_FACTOR * this.model.speed;
 					}
-					
-					this.idling = false;
-				}
-			
-			, idle: function ()
-				{
-					this.idling = true;
 				}
 			};
 });
