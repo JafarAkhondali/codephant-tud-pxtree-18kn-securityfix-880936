d032c79638d74f60aaf27471618f2e8f3a86bed5
André Arnold <andre.arnold@japaninleipzig.de>
2014-06-11 15:32:55 +0200

    add: Level unloading. add: already loaded levels will be unloaded on level loading and will spawn the ship in the new level according to its exit spot.

--- src/js/data.levels.js
+++ src/js/data.levels.js
@@ -4,7 +4,7 @@ namespace("PXTree.AchtzehnKnoten.Data.Levels",
 [ { spots:
 		[ { x: 50, y: 325
 			, reachable: [1, 2]
-			, start: 'west'
+			, start: { dir: 'west' }
 			}
 		
 		, { x: 105, y: 123
@@ -24,10 +24,38 @@ namespace("PXTree.AchtzehnKnoten.Data.Levels",
 			, event: { name: "pirate_privateer" } }
 		
 		, { x: 477, y: 347
-			, end:'south'
+			, end: { dir: 'south', to: 1 }
 			, event: { tags: ["open_sea", "atlantic"] } }
 		]
 	}
+
+, { spots:
+	[ { x: 50, y: 325
+		, reachable: [1, 2]
+		, start: { dir: 'north' }
+		}
+	
+	, { x: 105, y: 123
+		, reachable: [3]
+	, event: { tags: ["open_sea"] } }
+	
+	, { x: 175, y: 486
+		, reachable: [3], type: "island"
+			, event: { tags: ["island"] } }
+	
+	, { x: 289, y: 257, type: "water"
+		, reachable: [4, 5]
+	, event: { tags: ["open_sea"] } }
+	
+	, { x: 408, y: 105
+		, reachable: [5]
+	, event: { name: "pirate_privateer" } }
+	
+	, { x: 477, y: 347
+		, end: { dir: 'east', to: 0 }
+	, event: { tags: ["open_sea", "atlantic"] } }
+	]
+}
 ]
 
 );
