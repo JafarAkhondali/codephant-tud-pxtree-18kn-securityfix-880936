3244f907f81d0bef89d30f291f82daff01eefc54
favy <favy@Pixel Tree>
2014-06-29 12:54:26 +0200

    Almanach Update: Content pages

--- src/js/almanach.js
+++ src/js/almanach.js
@@ -6,12 +6,17 @@ namespace("PXTree.AchtzehnKnoten", function(AK)
 	var paper = null;
 	var exit = null;
 	var txtGrp;
+	var temp_title; 
+	var temp_page = 0;
+	var self;
+	var page_flag = 0;
 	
 	AK.Almanach = function Almanach (parentCtrl) {
 		this.parent = parentCtrl;
 		this.top = parentCtrl.top;
 		this.play = parentCtrl;
 		this.game = parentCtrl.game;
+		this.self = this;
 		this.preload();
 		this.create();
 
@@ -24,7 +29,6 @@ namespace("PXTree.AchtzehnKnoten", function(AK)
 	};
 	
 	AK.Almanach.prototype.create = function create(){
-		alm_view = this.game.make.group();
 		data_almanach = AK.Data.Almanach;
 		this.makeBackground();
 		alm_view = this.game.add.group();
@@ -38,29 +42,31 @@ namespace("PXTree.AchtzehnKnoten", function(AK)
 
 	
 	AK.Almanach.prototype.openAlmanach = function openAlmanach(){
-
-		var index_page;
 		paper.visible = true;
 		exit.visible = true;
+		page_flag = 0;
 		var titles = [];
 		var count = 0;
-		var title, text_img;
+		var title, text_im;
 		var text = "Keine gültigen Daten gefunden";
-		text.inputEnabled = true;
+
+		
 		var textstyle = Config.StatPaper.TextStyle;
 		var headline = this.game.add.text(Config.Left + 150, 80+count, 'Almanach - Index', textstyle);
 		txtGrp.add(headline);
 		for(var i=0; i<data_almanach.length;i++) {
 			count+=30;
 			if(text == null) text = "Keine gültigen Daten gefunden";
+			title = data_almanach[i].title;
+			//textbutton.events.onInputOver.add(mouseInHandler, this);
+			//textbutton.events.onInputOut.add(mouseOutHandler, this);
+			this.createButton(title, Config.Left + 50, 100+count);
 			text = this.game.make.text(Config.Left + 50, 100+count, data_almanach[i].title, textstyle);
 			txtGrp.add(text);
-			//text.events.onInputOver.add(mouseInHandler, this);
-			//text.events.onInputOut.add(mouseOutHandler, this);
-			//text.events.onInputDown.add(this.openPage(title,0), this);
 		};
 		txtGrp.setAll('visible','true');
-;
+		
+
 		
 
 	};
@@ -80,20 +86,44 @@ namespace("PXTree.AchtzehnKnoten", function(AK)
 	
 	
 	AK.Almanach.prototype.openPage = function (title, page) {
-		var page_data = null;
-		var cpp = 3; //Content Per Page: number of paragraphs/pictures per page
+    	txtGrp.removeAll();
+    	alm_view.removeAll();
+    	page_flag = 1;
+    	var self = this;
+		var page_data = data_almanach[0].content;
+		var cpp = 5; //Content Per Page: number of paragraphs/pictures per page
 		for(var i=0;i<data_almanach.length;i++){
 			if(data_almanach[i].title==title) {page_data = data_almanach[i].content; break; }
 		}
-		
+		var count = 0;
+		var textstyle = Config.StatPaper.TextStyle;
 		var content_text;
-		for(var i=0;i<cpp;i++){
-			if(page_data[i+page*cpp].type = 'paragraph') {
-				content_text+=page_data[i+page*cpp].text;
+		var headline = this.game.add.text(Config.Left + 150, 50+count, title + ' (Seite ' + (page+1) +')', textstyle);
+		txtGrp.add(headline);
+		var from = page*cpp;
+		var upto = (cpp+page*cpp>page_data.length)? page_data.length-cpp*page : cpp;
+		for(var i=from;i<upto;i++){
+			if(page_data[i].type == 'paragraph') {
+				content_text = page_data[i].text;
+				var content_lines = self.splitText(content_text);
+				for(var j=0;j<content_lines.length;j++){
+					text = this.game.make.text(Config.Left + 50, 100+count, content_lines[j], textstyle);
+					txtGrp.add(text);
+					count+=20;
+				}
 			}
-			if(page_data[i+page*cpp].type = 'image') {
-				
+			if(page_data[i].type == 'image') {
+				content_text=page_data[i].url;
+				var img_name = 'image'+i;
+				//text = this.game.make.text(Config.Left + 50, 100+count, content_text, textstyle);
+				self.game.load.image(img_name,content_text);
+				alm_view.create(Config.Left + 50, 100+count,img_name);
+				console.log(img_name);
+				console.log(content_text);
+				//txtGrp.add(text);
+				count+=20;
 			}
+			
 		}
 		if(page_data.length>(page*cpp+cpp)){
 			//TODO: Link zur nächsten Seite   this.openPage(title, page+1);
@@ -107,12 +137,51 @@ namespace("PXTree.AchtzehnKnoten", function(AK)
 	};
 	
 	AK.Almanach.prototype.closeAlmanach = function(){
-    	paper.visible = false;
-    	exit.visible = false;
-    	txtGrp.setAll('visible','false');
     	txtGrp.removeAll();
+    	alm_view.removeAll();
+		if(page_flag == 0){
+		paper.visible = false;
+    	exit.visible = false; 
+    	txtGrp.setAll('visible','false'); 
+    	temp_page=0; }
+    	if(page_flag == 1) this.openAlmanach();
     	
     }
+	
+	AK.Almanach.prototype.createButton = function(title, x, y) {
+		var textbutton = alm_view.create(x,y,null);
+		var self = this;
+		textbutton.inputEnabled = true;
+		textbutton.events.onInputDown.add(function() {
+			self.openPage(title, 0);
+				});
+		
+	};
+	
+	AK.Almanach.prototype.splitText = function (text){
+		var lines = [];
+		var txt = text.split(" ");
+		var line = "";
+		for(var i=0;i<txt.length;i++){
+			if(txt[i].length>45) {
+				var longtxt = txt[i].match(/[\s\S]{1,45}/g) || [];
+				for(var j=0;j<longtxt.length;j++) {lines.push(longtxt[j]);}
+			}
+			else{
+				var temp_line=line;
+				temp_line+=txt[i]+' ';
+				if(temp_line.length>50){
+					lines.push(line);
+					line = "";
+				}
+				line+=txt[i]+' ';
+			}
+		}
+		if(line!="") lines.push(line);
+		lines.push('');
+		return lines;
+	}
+	
 	
 	mouseInHandler = function ()
 	{
--- src/js/data.almanach.js
+++ src/js/data.almanach.js
@@ -4,7 +4,7 @@ namespace("PXTree.AchtzehnKnoten.Data.Almanach",
 		title:"Geographie",
 		content:[	{
 						type:'image', 
-						url:"assets\chars\captain-british-large.png"
+						url:"assets/icons/map-schiff.png"
 					},
 					{
 						type:'paragraph',
@@ -12,22 +12,30 @@ namespace("PXTree.AchtzehnKnoten.Data.Almanach",
 					},
 					{
 						type:'paragraph',
-						text:"Blabla bla vblalblablabla"
+						text:"1_abcdefg_2_abcdefg_3_abcdefg_3_abcdefg_4_abcdefg_5_abcdefg_6_abcdefg_7_abcdefg_8_abcdefg_9_abcdefg_10_abcdefg_11_abcdefg_"
+					},
+					{
+						type:'paragraph',
+						text:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
 					}
-		         ]
+		]
 	},
 	
 	{
-		title:"Hauptschule",
+		title:"Nautik",
 		content:[	{
 						type:'image', 
-						url:"assets\chars\captain-british-large.png"
+						url:"assets/icons/map-schiff.png"
 					},
 					{
 						type:'paragraph',
-						text:"Blabla bla vblalblablabla"
+						text:"Nautik, Paragraph 2"
 					},
-		         ]
+					{
+						type:'image', 
+						url:"assets/icons/map-schiff.png"
+					},
+		]
 	},
 	
 ]
