8a094da128cdcc0bac7b676be29ad4b7b4303cb4
favy <favy@Pixel Tree>
2014-07-06 20:00:00 +0200

    Tutorial finished, but opens on every start now

--- src/js/data.tutorial.js
+++ src/js/data.tutorial.js
@@ -1,14 +1,14 @@
-namespace("PXTree.AchtzehnKnoten.Data.Almanach",
+namespace("PXTree.AchtzehnKnoten.Data.Tutorial",
 
 [ 	{
 		title:"Willkommen bei 18 Knoten",
 		content:[	{
 						type:'paragraph',
-						text:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+						text:"Willkommen in der Welt von 18 Knoten, wo du als Entdecker durch die Weltmeere segelst und neue Welten erkundest! Spielend lernst du über Schifffahrt, Geschichte, Geographie und viele andere spannende Themen!"
 					},
 					{
 						type:'paragraph',
-						text:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+						text:"Dieses Tutorial wird dich in die grundsätzliche Spielmechanik einweisen. Solltest du bereits mit dem Spiel 18 Knoten vertraut sein, kannst du das Tutorial jederzeit mit dem roten Pfeil in der Ecke dieses Blattes."
 					}
 		]
 	},
@@ -17,8 +17,72 @@ namespace("PXTree.AchtzehnKnoten.Data.Almanach",
 		title:"Spielprinzip",
 		content:[	{
 						type:'paragraph',
-						text:"Nautik, Paragraph 2"
+						text:"Das Spielprinzip hinter 18 Knoten ist denkbar einfach: Mit deinem Schiff bewegst du dich auf der Karte von Punkt zu Punkt. Du kannst benachbarte Punkte besuchen, indem du sie anklickst. Die roten Pfeile am Rand der Karte bringen dich zur Weltkarte. Dort kannst du das nächste Level deiner Reise auswählen."
 					},
+		         	{
+						type:'paragraph',
+						text:"Am Ende deiner Reise wirst du ein neues Ziel entdecken. Dabei wird dir der Erfolg deiner Reise -hauptsächlich bestimmt durch die Menge an gesammelten Gold und deiner Moral- angezeigt."
+					}
+		]
+	},
+	{
+		title:"Events und Stats",
+		content:[	{
+		         		type:'paragraph',
+		         		text:"Sobald dein Schiff einen Punkt erreicht öffnet sich ein Event. Dabei wirst du als Kapitän vor verschiedene Situationen gestellt, in denen du dein Wissen und Denkvermögen unter Beweis stellen musst. Jedes Events beeinflusst deine Stats, also versuche nicht zu viele falsche Entscheidungen zu treffen, sonst ruht dein Schiff und du bald auf dem Boden des Meeres."     
+					},
+					{
+						type:'paragraph',
+						text:"Als Stats werden die wichtigsten Eigenschaften deines Schiffes bezeichnet. Du findest sie auf dem Schreibtisch neben der Karte auf dem weißen Papier. Die wichtigsten Stats werden auf der folgenden Seite erklärt."
+					},				
+		]
+	},
+	{
+		title:"Stats - Teil 1",
+		content:[	{
+						type:'paragraph',
+						text:"Mannschaft bezeichnet die Größe deiner Mannschaft. Sinkt deine Mannschaft auf null, geht dein Schiff unter."
+					},
+					{
+						type:'paragraph',
+						text:"Waffenstärke gibt Auskunft über die Bewaffnung deines Schiffes. Sie ist wichtig, um im Kampf gegen Piraten und andere Gefahren siegreich hervorzugehen."
+					},
+					{
+						type:'paragraph',
+						text:"Nahrung dient als Energiequelle bzw. Treibstoff für dein Schiff. Jeder Zug verbraucht eine bestimmte Menge Nahrung. Versuche knapp werdende Nahrung bei Events aufzufüllen."
+					},			
+		]
+	},
+	{
+		title:"Stats - Teil 2",
+		content:[	{
+						type:'paragraph',
+						text:"Gold ist die Währung der vergangenen Jahrhunderte und ein wichtiger Grund für dein Unternehmen. Nutze sie zum Handeln oder sammle sie für ein besseres Endergebnis."
+					},
+					{
+						type:'paragraph',
+						text:"Moral ist die kleine rote Leiste unter dem Bild des Kapitäns. Sie zeigt die Stimmung deiner Mannschaft. Erweist du dich als unfähiger Kapitän sinkt deine Moral und deine Mannschaft könnte meutern."
+					},					
+		]
+	},
+	{
+		title:"Almanach und Optionen",
+		content:[	{
+						type:'paragraph',
+						text:"Der Almanach ist das zentrale Lerninstrument dieses Spiels. Du kannst ihn jederzeit öffnen, indem dem du das rote Buchsymbol mit dem kleinen i auf dem Schreibtisch anklickst. Dort findest du alle Informationen, die du zum erfolgreichen Bestehen deiner Mission brauchst. Falls du bei einem Event nicht weiter weißt, öffne einfach den Almanach um dein Wissen zu vergrößern. Der Almanach ist kategorisch eingeteilt, drücke einfach auf das gesuchte Themen um zum Inhaltstext zu gelangen."
+					},
+		         	{
+						type:'paragraph',
+						text:"Neben dem Almanach findest du auf dem Schreibtisch den Anker, der dich zurück ins Hauptmenü bringt und TODO:SOundsymbol, welches das Spiel stumm schaltet."
+					},
+		]
+	},
+	{
+		title:"Viel Spaß!",
+		content:[	{
+						type:'paragraph',
+						text:"So, das war es, das Tutorial ist beendet! Jetzt klicke auf den roten Pfeil und stürze dich in die Welt von 18 Knoten!"
+					}
 		]
 	},
 	
--- src/js/tutorial.js
+++ src/js/tutorial.js
@@ -4,6 +4,7 @@ namespace("PXTree.AchtzehnKnoten", function (AK)
 	var txtGrp;
 	var viewGrp;
 	var tut_data = AK.Data.Tutorial;
+	var textstyle = Config.TextStyle;
 	
 	//TODO: Tutorial
 	
@@ -24,18 +25,20 @@ namespace("PXTree.AchtzehnKnoten", function (AK)
 		viewGrp = this.game.add.group();
 		txtGrp = this.game.add.group();
 		this.makeBackground();
+		viewGrp.setAll('visible','false');
+		viewGrp.visible = false;
 	}
 	
 	
 	AK.Tutorial.prototype.makeBackground = function (){
 		paper = this.game.add.sprite(500, 275, 'paper');
 		paper.anchor.set(0.5);
-		paper.visible = true;
+		paper.visible = false;
 		paper.scale.set(2.6);
-		exit = this.game.add.sprite(270, 480, 'exit');
+		exit = this.game.add.sprite(250, 500, 'exit');
 	    exit.anchor.set(0.5);
 	    exit.scale.set(1);
-	    exit.visible = true;
+	    exit.visible = false;
 	    exit.inputEnabled = true;
 	    exit.events.onInputDown.add(
 	    		this.closeTutorial,this);
@@ -43,22 +46,101 @@ namespace("PXTree.AchtzehnKnoten", function (AK)
 	    viewGrp.add(exit);
 	};
 	
+	AK.Tutorial.prototype.createButton = function(title, x, y, textlength, obj) {
+		var textbutton = this.game.add.tileSprite(x-5,y-5,textlength*10, y+5, null);
+		viewGrp.add(textbutton);
+		var self = this;
+		textbutton.inputEnabled = true;
+		textbutton.events.onInputDown.add(function() {
+			self.openTutorial(title);
+				});
+		textbutton.events.onInputOver.add(function() {
+			obj.fill='#ff774b';
+		});
+		textbutton.events.onInputOut.add(function() {
+			obj.fill='black';
+		});
+	};
+	
+	AK.Tutorial.prototype.splitText = function (text){
+		var lines = [];
+		var txt = text.split(" ");
+		var line = "";
+		for(var i=0;i<txt.length;i++){
+			if(txt[i].length>100) {
+				var longtxt = txt[i].match(/[\s\S]{1,80}/g) || [];
+				for(var j=0;j<longtxt.length;j++) {lines.push(longtxt[j]);}
+			}
+			else{
+				var temp_line=line;
+				temp_line+=txt[i]+' ';
+				if(temp_line.length>65){
+					lines.push(line);
+					line = "";
+				}
+				line+=txt[i]+' ';
+			}
+		}
+		if(line!="") lines.push(line);
+		lines.push('');
+		return lines;
+	}
+	
 	AK.Tutorial.prototype.closeTutorial = function(){
     	txtGrp.removeAll();
     	viewGrp.removeAll();
-		/*if(page_flag == 0){
-		paper.visible = false;
-    	exit.visible = false; 
-    	txtGrp.setAll('visible','false'); 
-    	temp_page=0; }*/
-    	//if(page_flag == 1) this.openTutorial();
+    	this.parent.first_start_flag = 0;
     	
     }
 	
-	AK.Tutorial.prototype.openTutorial = function openTutorial(){
-		var headline = this.game.add.text(500, 80, 'Willkommen bei 18 Knoten', Config.Textstyle);
+	AK.Tutorial.prototype.openTutorial = function openTutorial(page){
+		this.parent.first_start_flag = 0;
+		var pagetitle, text, title, pagenum;
+		txtGrp.removeAll();
+		(!page)? pagetitle='Willkommen bei 18 Knoten' : pagetitle = tut_data[page].title;
+		var headline = this.game.add.text(500, 100, pagetitle, textstyle);
 		headline.anchor.set(0.5);
 		txtGrp.add(headline);
-		this.closeTutorial();
+		viewGrp.setAll('visible','true');
+		txtGrp.setAll('visible','true');
+		viewGrp.visible = true;
+		txtGrp.visible = true;
+    	var self = this;
+		var page_data = tut_data[0].content;
+		pagenum = page;
+    	page_data = tut_data[pagenum].content;
+		var count = 0;
+		var content_text;
+		var content_lines = [];
+		headline.fill='black';
+		txtGrp.add(headline);
+		for(var i=0;i<page_data.length;i++){
+			if(page_data[i].type == 'paragraph') {
+				content_text = page_data[i].text;
+				if(content_lines.length <= 0 || typeof content_lines == 'undefined') content_lines = self.splitText(content_text);
+				else {
+					content_lines.push.apply(content_lines, self.splitText(content_text));
+				}
+				
+			}
+		}
+		
+		for(var j=0;j<content_lines.length;j++){
+				var contenttext = this.game.make.text(270, 140+count, content_lines[j], textstyle);
+				contenttext.fill = 'black';
+				txtGrp.add(contenttext);
+				count+=25;
+			}
+		if(page<tut_data.length-1){
+			var next_link = this.game.add.text(630, 470, 'Nächste Seite', textstyle);
+			txtGrp.add(next_link);
+			this.createButton(pagenum+1, 630, 480, tut_data[pagenum+1].title, next_link);
+		}
+		if(page>0){
+			var prev_link = this.game.add.text(300, 470, 'Vorherige Seite', textstyle);
+			txtGrp.add(prev_link);
+			this.createButton(pagenum-1, 300, 480, tut_data[pagenum-1].title, prev_link);
+		}
+		txtGrp.setAll('visible','true');
 	}
 });
