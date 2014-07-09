b5fdeefd1fc00a483d04d5b4eff611260ba85f77
skijunk <gregor_stopp@web.de>
2014-07-09 21:05:05 +0200

    almanach geo_paragraphs

--- src/js/data.almanach.js
+++ src/js/data.almanach.js
@@ -1,34 +1,84 @@
 namespace("PXTree.AchtzehnKnoten.Data.Almanach",
 
 [ 	{
-		title:"Geographie",
+		title:"Erdkunde",
 		content:[	{
-						type:'paragraph',
-						text:"Blabla bla vblalblablabla"
+						type:'paragraph',   //Absatz zu Frage 1
+						text:"Das Kap Agulhas liegt bei zirka 35° Süd 20° Ost. Diese Landspitze ist der südlichste Punkt des Afrikanischen Kontinents und nicht, wie oft angenommen das Kap der Guten Hoffnung. Es trennt den Atlantischen vom Indischen Ozean."
 					},
-					{
-						type:'paragraph',
-						text:"1_abcdefg_2_abcdefg_3_abcdefg_3_abcdefg_4_abcdefg_5_abcdefg_6_abcdefg_7_abcdefg_8_abcdefg_9_abcdefg_10_abcdefg_11_abcdefg_"
+					
+                    {
+						type:'paragraph',   //2
+						text:"Das Kap Farvel liegt bei knapp 60° nördlicher Breite. Es ist die südlichste Landspitze von Grönland, die größte Insel der Erde. Sie zählt geologisch zu Nordamerika."
 					},
-					{
-						type:'paragraph',
-						text:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+                 
+                    {
+                        type:'paragraph',   //3 & 4
+                        text:"Die Erde dreht sich um die Sonne. Dabei dreht sie sich ebenso um ihre eigene Achse. Die Eigenrotation erfolgt um eine Achse welche um 66,6° zur Rotationsebene der Drehung um die Sonne geneigt ist. Dadurch entsteht innerhalb der Polarregionen, welche den Bereich nördlich/ südlich von 66,6° nördlicher bzw. südlicher Breite defininert, je nach Jahreszeit der Polartag oder die Polarnacht. Im Norden entsteht so während des Winters die Polarnacht."
+                    },
+                 
+                    {
+                        type:'paragraph',   //5
+                        text:"Innerhalb der Ozeane gibt es warme und kalte Meereströmungen. Eine der warmen Strömungen ist der Golfstrom. Dieser entsteht im Golf von Mexiko und fließt im Atlantischen Ozean in Richtung Nordpolarmeer. Dort wird er zum sogenannten Nordpolarstrom."
+                    },
+                 
+                    {
+                        type:'paragraph',   //6
+                        text:"Die Insel Grönland ist zu großen Teilen vergletschert. Dieser Eispanzer wird als Grönländisches Eisschild bezeichnet. Die Gletscher sind die größten Süßwasserspeicher der Erde und so liegen in Grönland die umfangreichsten Reserven der Erde außerhalb der Antatrktis."
+                    },
+                 
+                    {
+                        type:'paragraph',   //7
+                        text:"Kattegat und Skagerrak sind Meeresgebiete zwischen der Ostsee und der Nordsee. Sie schaffen eine Verbindung zwischen beiden Meeren und eine Trennung zwischen Skandinavien und Jütland. Das Skagerrak liegt weiter westlich als das Kattegat."
+                    },
+                 
+                    {
+                        type:'paragraph',   //8
+                        text:"Spitzbergen ist eine Inselgruppe mit über 400 einzelnen Inseln. Östlich von Spitzbergen liegt die Barentssee, teilweise ein Teil des Aktischen Ozeans und im Westen liegt die Grönlandsee, ebenso teilweise ein Teil des Arktischen Ozean. Südlich des Arktischen Ozean zählen beide Meeresgebiete zum Nordartlantik."
+                    },
+                 
+                    {
+                        type:'pargraph',    //9
+                        text:"Die Doggerbank ist eine Meeresuntiefe zwischen England, Norwegen und dem Europäischen Festland. Während der letzten Eiszeit, als der Meeresspiegel in etwa 100m tiefer lag, war diese Untiefe ein Landgebiet. Der Ärmelkanal war ebenso verlandet. Es war also eine Landverbindung zwischen England und dem Festland vorhanden."
+                    },
+                 
+                    {
+						type:'paragraph',   //10
+						text:"Das Kap Hoorn trennt den Pazifischen vom Atlantischen Ozean. Es ist die südlichste Landspitze des Südamerikanischen Kontinents und ein Teil des Archipels Feuerland."
 					},
-					{
-						type:'paragraph',
-						text:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-					}
-		]
+				
+                    {
+                        type:'paragraph',   //11
+                        text:"Die Kanarischen Inseln sind eine Inselgruppe westlich von Afrika. Sie liegen in etwa 100- 500km entfernt von Marokko. Sie gehören zu einer Region mit dem Namen Makaronesien. Dazu gehören außerdem die Azoren, Madeira, die Inselgruppe Ilhas Selvagens und die Kapverdischen Inseln"
+                    },
+                 
+                    {
+                        type:'paragraph',   //12 & 13
+                        text:"Die Erde ist ein rotierender Körper. Jeder bewegte Körper auf einem rotierenden Körper erfährt eine Kraft, welche nicht in Rotationsrichtung zeigt. Diese Kraft heißt Corioliskraft. Sie sorgt dafür, dass nördlich und südlich der Innertropischen Konvergenzzone eine vorherschende Windrichtung stets eine Ostkomponente hat."
+                    },
+                 
+                    /*{
+                        type:'paragraph',   //14
+                        text:"Die Karibik ist eine Region aus den im Karibischem Meer liegend"
+                    },*/
+                 
+				]
 	},
 	
-	{
-		title:"Nautik",
+	/*{
+		title:"Geschichte",
 		content:[	{
 						type:'paragraph',
-						text:"Nautik, Paragraph 2"
+						text:"Geschichte"
 					},
-		]
-	},
+					{
+						type:'paragraph',
+						text:"geschhcihte2"
+					},
+				]
+	},*/,
+	
+	
 	
 ]
 );
