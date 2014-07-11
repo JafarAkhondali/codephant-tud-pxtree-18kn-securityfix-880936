3ddd751a32b2b00e35f196ae1c15224c76af59a2
skijunk <gregor_stopp@web.de>
2014-07-11 19:15:09 +0200

    almanac more paragraphs

--- src/js/data.almanach.js
+++ src/js/data.almanach.js
@@ -1,84 +1,173 @@
 namespace("PXTree.AchtzehnKnoten.Data.Almanach",
 
 [ 	{
-		title:"Erdkunde",
-		content:[	{
-						type:'paragraph',   //Absatz zu Frage 1
+		title:"Kartografie der Inseln und Landmarken",   // Geography almanac paragraphs
+		content:
+                [
+                    {
+						type:'paragraph',   //  almanac paragraph for geo question 1
 						text:"Das Kap Agulhas liegt bei zirka 35° Süd 20° Ost. Diese Landspitze ist der südlichste Punkt des Afrikanischen Kontinents und nicht, wie oft angenommen das Kap der Guten Hoffnung. Es trennt den Atlantischen vom Indischen Ozean."
 					},
 					
                     {
-						type:'paragraph',   //2
+						type:'paragraph',   //  2
 						text:"Das Kap Farvel liegt bei knapp 60° nördlicher Breite. Es ist die südlichste Landspitze von Grönland, die größte Insel der Erde. Sie zählt geologisch zu Nordamerika."
 					},
                  
                     {
-                        type:'paragraph',   //3 & 4
-                        text:"Die Erde dreht sich um die Sonne. Dabei dreht sie sich ebenso um ihre eigene Achse. Die Eigenrotation erfolgt um eine Achse welche um 66,6° zur Rotationsebene der Drehung um die Sonne geneigt ist. Dadurch entsteht innerhalb der Polarregionen, welche den Bereich nördlich/ südlich von 66,6° nördlicher bzw. südlicher Breite defininert, je nach Jahreszeit der Polartag oder die Polarnacht. Im Norden entsteht so während des Winters die Polarnacht."
+                        type:'paragraph',   //  3 & 4 & 18
+                        text:"Die Erde dreht sich um die Sonne. Dabei dreht sie sich ebenso um ihre eigene Achse. Die Eigenrotation erfolgt um eine Achse welche um 66,6° zur Rotationsebene der Drehung um die Sonne geneigt ist. Dadurch entsteht innerhalb der Polarregionen, welche den Bereich nördlich/ südlich von 66,6° nördlicher bzw. südlicher Breite defininert, je nach Jahreszeit der Polartag oder die Polarnacht. Im Norden entsteht so während des Winters die Polarnacht und im Süden der Polartag. Im Sommer ist es aufgrund der Rotation um die Sonne umgekehrt. In den Tropen kommt es aufgrund dieser Naturerscheinnug zu einer periodischen Bewegung des Sonnenzenits."
                     },
                  
                     {
-                        type:'paragraph',   //5
+                        type:'paragraph',   //  5 & 16
                         text:"Innerhalb der Ozeane gibt es warme und kalte Meereströmungen. Eine der warmen Strömungen ist der Golfstrom. Dieser entsteht im Golf von Mexiko und fließt im Atlantischen Ozean in Richtung Nordpolarmeer. Dort wird er zum sogenannten Nordpolarstrom."
                     },
                  
                     {
-                        type:'paragraph',   //6
+                        type:'paragraph',   //  6
                         text:"Die Insel Grönland ist zu großen Teilen vergletschert. Dieser Eispanzer wird als Grönländisches Eisschild bezeichnet. Die Gletscher sind die größten Süßwasserspeicher der Erde und so liegen in Grönland die umfangreichsten Reserven der Erde außerhalb der Antatrktis."
                     },
                  
                     {
-                        type:'paragraph',   //7
+                        type:'paragraph',   //  7
                         text:"Kattegat und Skagerrak sind Meeresgebiete zwischen der Ostsee und der Nordsee. Sie schaffen eine Verbindung zwischen beiden Meeren und eine Trennung zwischen Skandinavien und Jütland. Das Skagerrak liegt weiter westlich als das Kattegat."
                     },
                  
                     {
-                        type:'paragraph',   //8
+                        type:'paragraph',   //  8
                         text:"Spitzbergen ist eine Inselgruppe mit über 400 einzelnen Inseln. Östlich von Spitzbergen liegt die Barentssee, teilweise ein Teil des Aktischen Ozeans und im Westen liegt die Grönlandsee, ebenso teilweise ein Teil des Arktischen Ozean. Südlich des Arktischen Ozean zählen beide Meeresgebiete zum Nordartlantik."
                     },
                  
                     {
-                        type:'pargraph',    //9
+                        type:'pargraph',    //  9
                         text:"Die Doggerbank ist eine Meeresuntiefe zwischen England, Norwegen und dem Europäischen Festland. Während der letzten Eiszeit, als der Meeresspiegel in etwa 100m tiefer lag, war diese Untiefe ein Landgebiet. Der Ärmelkanal war ebenso verlandet. Es war also eine Landverbindung zwischen England und dem Festland vorhanden."
                     },
                  
                     {
-						type:'paragraph',   //10
+						type:'paragraph',   //  10
 						text:"Das Kap Hoorn trennt den Pazifischen vom Atlantischen Ozean. Es ist die südlichste Landspitze des Südamerikanischen Kontinents und ein Teil des Archipels Feuerland."
 					},
 				
                     {
-                        type:'paragraph',   //11
+                        type:'paragraph',   //  11
                         text:"Die Kanarischen Inseln sind eine Inselgruppe westlich von Afrika. Sie liegen in etwa 100- 500km entfernt von Marokko. Sie gehören zu einer Region mit dem Namen Makaronesien. Dazu gehören außerdem die Azoren, Madeira, die Inselgruppe Ilhas Selvagens und die Kapverdischen Inseln"
                     },
                  
                     {
-                        type:'paragraph',   //12 & 13
+                        type:'paragraph',   //  12 & 13
                         text:"Die Erde ist ein rotierender Körper. Jeder bewegte Körper auf einem rotierenden Körper erfährt eine Kraft, welche nicht in Rotationsrichtung zeigt. Diese Kraft heißt Corioliskraft. Sie sorgt dafür, dass nördlich und südlich der Innertropischen Konvergenzzone eine vorherschende Windrichtung stets eine Ostkomponente hat."
                     },
                  
-                    /*{
-                        type:'paragraph',   //14
-                        text:"Die Karibik ist eine Region aus den im Karibischem Meer liegend"
-                    },*/
+                    {
+                        type:'paragraph',   //  14
+                        text:"Die Karibik ist eine Region, welche aus Inseln und Inselgruppen besteht. Im Westen liegt der Golf von Mexiko und im Süden das Karibische Meer. Die größten Inseln gehören zu der Gruppe der Großen Antillen. Dazu zählen unter anderem Kuba, Jamaika und Puerto Rico."
+                    },
+                 
+                    {
+                        type:'paragraph',  //   15
+                        text:"Die Ozeane sind durchzogen von Tiefseerinnen, welche an Stellen entstanden sind, wo sich im Meer tektonische Platten treffen. Der Atacamagraben ist durch Subduktion der Nasca- Platte, welche westlich von Südamerika liegt, unter die Südamerikanische Platte entstanden. Während des Subduktionsprozesses schiebt sich eine Platte unter die Andere. Dabei entsteht eine Tiefseerinne."
+                    },
+                 
+                    {
+                        type:'paragraph',   //  17
+                        text:"Eine Inselgruppe ist eine Ansammlung von mehreren Inseln, welche geologisch die selbe Entstehungsgeschichte haben oder aufgrund politischer, historischer oder kulureller Gründe zusammengefasst werden. Beispielsweise unterscheiden sich die Friesischen Inseln in ihrer Entstehung, werden aber dennoch als Gruppe aufgefasst. Je nach topografischer Anordnung kann man Inselketten, Inselbögen, Doppelinseln und Atolle unterscheiden. Das Atoll hat außerdem die Eigenschaft, dass es von vulkanischem Ursprunges ist."
+                    },
+                 
+                    {
+                        type:'paragraph',   //  19
+                        text:"Die Antillen bilden mit der Karibischen See, dem Golf von Mexiko und dem Atlantischen Ozean ein Archipel im Osten des Amerikanschischen Kontinents. Es werden zwei Untergruppen unterschieden: die Großen und die Kleinen Antillen, wovon die Ersten eine Gruppe bilden, welche bis in den Golf von Mexiko hineinreichen. Zu den größten Inseln gehören Kuba, Jamaica und Hispaniola. Die Kleinen Antillen erstrecken sich dagegen bis etwa 30 km an den Südamerikanischen Kontinent heran. Von den Westindischen Inseln spricht man, wenn außerdem die Bahamas hinzugezählt werden."
+                 
+                    },
+                 
+                    {
+                        type:'paragraph',   //  20
+                        text:"Die Insel Neufundland liegt auf dem Nordamerikanischen Kontinent im Mündungsgebiet des Sankt-Lorenz-Stromes und reicht im Westen bis in den Sankt-Lorenz-Golf hinein."
+                    },
                  
 				]
 	},
 	
-	/*{
-		title:"Geschichte",
-		content:[	{
+	{
+		title:"Geschichte der Antike",
+		content:
+                [
+                    {
 						type:'paragraph',
-						text:"Geschichte"
+						text:"Geschichte der Antike 1"
 					},
+                 
 					{
 						type:'paragraph',
-						text:"geschhcihte2"
+						text:"Geschichte der Antike 2"
 					},
 				]
-	},,*/
-	
-	
-	
+	},
+ 
+    {
+        title:"Geschichte des Mittelalters",
+        content:
+                [
+                    {
+                        type:'paragraph',
+                        text:"Geschichte des Mittelalters 1"
+                    },
+          
+                    {
+                        type:'paragraph',
+                        text:"Geschichte des Mittelalters 2"
+                    },
+                ]
+    },
+ 
+    {
+        title:"Geschichte der Renaissance",
+        content:
+                [
+                    {
+                        type:'paragraph',
+                        text:"Geschichte der Renaissance 1"
+                    },
+  
+                    {
+                        type:'paragraph',
+                        text:"Geschichte der Renaissance 2"
+                    },
+                 ]
+    },
+
+    {
+        title:"Geschichte des 17. Jahrhunderts",
+        content:
+                [
+                    {
+                        type:'paragraph',
+                        text:"Geschichte des 17. Jahrhunderts 1"
+                    },
+ 
+                    {
+                        type:'paragraph',
+                        text:"Geschichte des 17. Jahrhunderts 2"
+                    },
+                 ]
+    },
+
+    {
+        title:"Geschichte des 18. Jahrhunderts",
+        content:
+                [
+                    {
+                        type:'paragraph',
+                        text:"Geschichte des 18. Jahrhunderts 1"
+                    },
+                 
+                    {
+                        type:'paragraph',
+                        text:" Geschichte des 18. Jahrhunderts 2"
+                    },
+                ]
+    },
+
+ 
 ]
 );
