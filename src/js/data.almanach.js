c6b17be2607bfc35268fac3260f890656ebafe18
skijunk <gregor_stopp@web.de>
2014-07-13 16:49:34 +0200

    almanac table of content

--- src/js/data.almanach.js
+++ src/js/data.almanach.js
@@ -1,7 +1,7 @@
 namespace("PXTree.AchtzehnKnoten.Data.Almanach",
 
 [ 	{
-		title:"Kartografie",   // Geography almanac paragraphs
+		title:"Topographie der Landmarken",   // Geography almanac paragraphs
 		content:
                 [
                     {
@@ -14,35 +14,21 @@ namespace("PXTree.AchtzehnKnoten.Data.Almanach",
 						text:"Das Kap Farvel liegt bei knapp 60° nördlicher Breite. Es ist die südlichste Landspitze von Grönland, die größte Insel der Erde. Sie zählt geologisch zu Nordamerika."
 					},
                  
-                    {
-                        type:'paragraph',   //  3 & 4 & 18
-                        text:"Die Erde dreht sich um die Sonne. Dabei dreht sie sich ebenso um ihre eigene Achse. Die Eigenrotation erfolgt um eine Achse welche um 66,6° zur Rotationsebene der Drehung um die Sonne geneigt ist. Dadurch entsteht innerhalb der Polarregionen, welche den Bereich nördlich/ südlich von 66,6° nördlicher bzw. südlicher Breite definiert, je nach Jahreszeit der Polartag oder die Polarnacht. Im Norden entsteht so während des Winters die Polarnacht und im Süden der Polartag. Im Sommer ist es aufgrund der Rotation um die Sonne umgekehrt. In den Tropen kommt es aufgrund dieser Naturerscheinung zu einer periodischen Bewegung des Sonnenzenits."
-                    },
                  
-                    {
-                        type:'paragraph',   //  5 & 16
-                        text:"Innerhalb der Ozeane gibt es warme und kalte Meeresströmungen. Eine der warmen Strömungen ist der Golfstrom. Dieser entsteht im Golf von Mexiko und fließt im Atlantischen Ozean in Richtung Nordpolarmeer. Dort wird er zum sogenannten Nordpolarstrom."
-                    },
                  
                     {
                         type:'paragraph',   //  6
                         text:"Die Insel Grönland ist zu großen Teilen vergletschert. Dieser Eispanzer wird als Grönländisches Eisschild bezeichnet. Die Gletscher sind die größten Süßwasserspeicher der Erde und so liegen in Grönland die umfangreichsten Reserven der Erde außerhalb der Antarktis."
                     },
                  
-                    {
-                        type:'paragraph',   //  7
-                        text:"Kattegat und Skagerrak sind Meeresgebiete zwischen der Ostsee und der Nordsee. Sie schaffen eine Verbindung zwischen beiden Meeren und eine Trennung zwischen Skandinavien und Jütland. Das Skagerrak liegt weiter westlich als das Kattegat."
-                    },
+                 
                  
                     {
                         type:'paragraph',   //  8
                         text:"Spitzbergen ist eine Inselgruppe mit über 400 einzelnen Inseln. Östlich von Spitzbergen liegt die Barentssee, teilweise ein Teil des Arktischen Ozeans und im Westen liegt die Grönlandsee, die ebenso teilweise ein Teil des Arktischen Ozeanes ist. Südlich des Arktischen Ozean zählen beide Meeresgebiete zum Nordatlantik."
                     },
                  
-                    {
-                        type:'pargraph',    //  9
-                        text:"Die Doggerbank ist eine Meeresuntiefe zwischen England, Norwegen und dem Europäischen Festland. Während der letzten Eiszeit, als der Meeresspiegel in etwa 100m tiefer lag, war diese Untiefe ein Landgebiet. Der Ärmelkanal war ebenso verlandet. Es war also eine Landverbindung zwischen England und dem Festland vorhanden."
-                    },
+                 
                  
                     {
 						type:'paragraph',   //  10
@@ -54,20 +40,13 @@ namespace("PXTree.AchtzehnKnoten.Data.Almanach",
                         text:"Die Kanarischen Inseln sind eine Inselgruppe westlich von Afrika. Sie liegen in etwa 100- 500km entfernt von Marokko. Sie gehören zu einer Region mit dem Namen Makaronesien. Dazu gehören außerdem die Azoren, Madeira, die Inselgruppe Ilhas Selvagens und die Kapverdischen Inseln"
                     },
                  
-                    {
-                        type:'paragraph',   //  12 & 13
-                        text:"Die Erde ist ein rotierender Körper. Jeder bewegte Körper auf einem rotierenden Körper erfährt eine Kraft, welche nicht in Rotationsrichtung zeigt. Diese Kraft heißt Corioliskraft. Sie sorgt dafür, dass nördlich und südlich der Innertropischen Konvergenzzone eine vorherschende Windrichtung stets eine Ostkomponente hat."
-                    },
                  
                     {
                         type:'paragraph',   //  14
                         text:"Die Karibik ist eine Region, welche aus Inseln und Inselgruppen besteht. Im Westen liegt der Golf von Mexiko und im Süden das Karibische Meer. Die größten Inseln gehören zu der Gruppe der Großen Antillen. Dazu zählen unter anderem Kuba, Jamaika und Puerto Rico."
                     },
                  
-                    {
-                        type:'paragraph',  //   15
-                        text:"Die Ozeane sind durchzogen von Tiefseerinnen, welche an Stellen entstanden sind, wo sich im Meer tektonische Platten treffen. Der Atacamagraben ist durch Subduktion der Nasca- Platte, welche westlich von Südamerika liegt, unter die Südamerikanische Platte entstanden. Während des Subduktionsprozesses schiebt sich eine Platte unter die Andere. Dabei entsteht eine Tiefseerinne."
-                    },
+                 
                  
                     {
                         type:'paragraph',   //  17
@@ -87,6 +66,50 @@ namespace("PXTree.AchtzehnKnoten.Data.Almanach",
                  
 				]
 	},
+ 
+    {
+        title:"Topografie der Gewässer und Meere",
+        content:
+                [
+                    {
+                        type:'paragraph',   //  7
+                        text:"Kattegat und Skagerrak sind Meeresgebiete zwischen der Ostsee und der Nordsee. Sie schaffen eine Verbindung zwischen beiden Meeren und eine Trennung zwischen Skandinavien und Jütland. Das Skagerrak liegt weiter westlich als das Kattegat."
+                    },
+                 
+                    {
+                        type:'paragraph',    //  9
+                        text:"Die Doggerbank ist eine Meeresuntiefe zwischen England, Norwegen und dem Europäischen Festland. Während der letzten Eiszeit, als der Meeresspiegel in etwa 100m tiefer lag, war diese Untiefe ein Landgebiet. Der Ärmelkanal war ebenso verlandet. Es war also eine Landverbindung zwischen England und dem Festland vorhanden."
+                    },
+                 
+                ]
+    },
+ 
+    {
+        title:"Navigation, Geodäsie und Physikalisches",
+        content:
+                [
+                    {
+                        type:'paragraph',   //  3 & 4 & 18
+                        text:"Die Erde dreht sich um die Sonne. Dabei dreht sie sich ebenso um ihre eigene Achse. Die Eigenrotation erfolgt um eine Achse welche um 66,6° zur Rotationsebene der Drehung um die Sonne geneigt ist. Dadurch entsteht innerhalb der Polarregionen, welche den Bereich nördlich/ südlich von 66,6° nördlicher bzw. südlicher Breite definiert, je nach Jahreszeit der Polartag oder die Polarnacht. Im Norden entsteht so während des Winters die Polarnacht und im Süden der Polartag. Im Sommer ist es aufgrund der Rotation um die Sonne umgekehrt. In den Tropen kommt es aufgrund dieser Naturerscheinung zu einer periodischen Bewegung des Sonnenzenits."
+                    },
+                
+                    {
+                        type:'paragraph',   //  5 & 16
+                        text:"Innerhalb der Ozeane gibt es warme und kalte Meeresströmungen. Eine der warmen Strömungen ist der Golfstrom. Dieser entsteht im Golf von Mexiko und fließt im Atlantischen Ozean in Richtung Nordpolarmeer. Dort wird er zum sogenannten Nordpolarstrom."
+                    },
+                 
+                    {
+                        type:'paragraph',   //  12 & 13
+                        text:"Die Erde ist ein rotierender Körper. Jeder bewegte Körper auf einem rotierenden Körper erfährt eine Kraft, welche nicht in Rotationsrichtung zeigt. Diese Kraft heißt Corioliskraft. Sie sorgt dafür, dass nördlich und südlich der Innertropischen Konvergenzzone eine vorherschende Windrichtung stets eine Ostkomponente hat."
+                    },
+                 
+                    {
+                        type:'paragraph',  //   15
+                        text:"Die Ozeane sind durchzogen von Tiefseerinnen, welche an Stellen entstanden sind, wo sich im Meer tektonische Platten treffen. Der Atacamagraben ist durch Subduktion der Nasca- Platte, welche westlich von Südamerika liegt, unter die Südamerikanische Platte entstanden. Während des Subduktionsprozesses schiebt sich eine Platte unter die Andere. Dabei entsteht eine Tiefseerinne."
+                    },
+                ]
+ 
+    },
 	
 	{
 		title:"Geschichte der Antike",
